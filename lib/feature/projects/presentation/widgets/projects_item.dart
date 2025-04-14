cec4ec7a766a4e1892c89c9c2fa756560c1c2a8d
Nacchofer31 <ignaciofer31@gmail.com>
2025-04-14 02:02:00 +0200

    fix: fixed project item width

--- lib/feature/projects/presentation/widgets/projects_item.dart
+++ lib/feature/projects/presentation/widgets/projects_item.dart
@@ -11,17 +11,14 @@ class ProjectItem extends StatelessWidget {
     final isVerySmall = Responsive.isVerySmall(context);
     return Card(
       child: Container(
-        padding: const EdgeInsets.all(12),
+        padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           children: [
             Row(
-              crossAxisAlignment: isVerySmall
-                  ? CrossAxisAlignment.center
-                  : CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
               children: [
                 SizedBox(
                   width: 56,
@@ -36,131 +33,127 @@ class ProjectItem extends StatelessWidget {
                   ),
                 ),
                 SizedBox(width: Responsive.maxSmallSpacing(context)),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        projectData.name.toUpperCase(),
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        projectData.type,
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      ),
-                      SizedBox(height: isVerySmall ? 0 : 8),
-                      Text.rich(
-                        TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'DESCRIPTION: ',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium
-                                  ?.copyWith(fontWeight: FontWeight.bold),
-                            ),
-                            TextSpan(
-                              text: projectData.description,
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: isVerySmall ? 0 : 8),
-                      Text.rich(
-                        TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'SKILLS: ',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium
-                                  ?.copyWith(fontWeight: FontWeight.bold),
-                            ),
-                            TextSpan(
-                              text: projectData.skills,
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: isVerySmall ? 0 : 8),
-                      Row(
-                        children: [
-                          Text(
-                            'Built with:',
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          SizedBox(
-                            width: 35,
-                            height: 35,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(12),
-                              child: Image.asset(
-                                projectData.techIcon,
-                                width: 35,
-                                height: 35,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        height: 500,
-                        padding: const EdgeInsets.all(20),
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: projectData.screenshots
-                              .map(
-                                (screenshot) => Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 20),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(12),
-                                    child: Image.asset(
-                                      height: 500,
-                                      screenshot,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                              )
-                              .toList(),
-                        ),
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            'Links:',
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          if (projectData.appStoreLink != null)
-                            LinkWidget(
-                                type: ProjectLinkType.appstore,
-                                link: projectData.appStoreLink!),
-                          if (projectData.playStoreLink != null)
-                            LinkWidget(
-                                type: ProjectLinkType.playstore,
-                                link: projectData.playStoreLink!),
-                          if (projectData.webLink != null)
-                            LinkWidget(
-                                type: ProjectLinkType.web,
-                                link: projectData.webLink!)
-                        ],
-                      ),
-                    ],
+                Flexible(
+                  child: Text(
+                    projectData.name.toUpperCase(),
+                    style: Theme.of(context).textTheme.headlineMedium,
                   ),
                 ),
               ],
+            ),
+            const SizedBox(height: 8),
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    projectData.type,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  SizedBox(height: isVerySmall ? 0 : 8),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'DESCRIPTION: ',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium
+                              ?.copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: projectData.description,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: isVerySmall ? 0 : 8),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'SKILLS: ',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium
+                              ?.copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: projectData.skills,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: isVerySmall ? 0 : 8),
+                  Row(
+                    children: [
+                      Text(
+                        'Built with:',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      const SizedBox(width: 5),
+                      SizedBox(
+                        width: 35,
+                        height: 35,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.asset(
+                            projectData.techIcon,
+                            width: 35,
+                            height: 35,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 500,
+                    padding: const EdgeInsets.all(20),
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: projectData.screenshots
+                          .map(
+                            (screenshot) => Padding(
+                              padding: const EdgeInsets.only(right: 20),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(12),
+                                child: Image.asset(
+                                  height: 500,
+                                  screenshot,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Links:',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      const SizedBox(width: 5),
+                      if (projectData.appStoreLink != null)
+                        LinkWidget(
+                            type: ProjectLinkType.appstore,
+                            link: projectData.appStoreLink!),
+                      if (projectData.playStoreLink != null)
+                        LinkWidget(
+                            type: ProjectLinkType.playstore,
+                            link: projectData.playStoreLink!),
+                      if (projectData.webLink != null)
+                        LinkWidget(
+                            type: ProjectLinkType.web,
+                            link: projectData.webLink!)
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
